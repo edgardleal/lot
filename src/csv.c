5ccc89daa9131f4339938f5aedec3b4aa9b1efc8
edgardleal <edgardleal@gmail.com>
2017-02-22 20:49:44 -0300

    Star implementation of csv features
      This feature will be used to read and write number, results
    statistcs etc.

---
+++ src/csv.c
@@ -0,0 +1,18 @@
+/**
+ *
+ * @author Edgard Leal <edgardleal@gmail.com> 
+ * @language C
+ * @libs stdio 
+ *
+ **/
+#ifndef CSV_C
+#define CSV_C
+#include <stdio.h>
+
+extern void split_csv(char *line, char **result)
+{
+
+}
+
+#endif
+/* vim: set expandtab tabstop=4 :*/
---
+++ src/csv.h
@@ -0,0 +1,9 @@
+#ifndef CSV_H
+#define CSV_H
+#include <stdio.h>
+#include <stdlib.h>
+
+extern void split_csv(char *, char**);
+
+#endif
+/* vim: set expandtab tabstop=4 :*/
