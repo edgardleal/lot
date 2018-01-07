d6ff72987af19bd18fd9732010c6b84affbccc97
edgardleal <edgardleal@gmail.com>
2018-01-07 10:37:15 -0300

    :pencil: Update documentation

--- docs/search/all_1.js
+++ docs/search/all_1.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['color_2eh',['color.h',['../color_8h.html',1,'']]],
-  ['command',['Command',['../structCommand.html',1,'']]],
+  ['command',['command',['../structcommand.html',1,'command'],['../structCommand.html',1,'Command']]],
   ['command_2ec',['command.c',['../command_8c.html',1,'']]],
   ['config',['config',['../structconfig.html',1,'']]],
   ['csv_2eh',['csv.h',['../csv_8h.html',1,'']]]
